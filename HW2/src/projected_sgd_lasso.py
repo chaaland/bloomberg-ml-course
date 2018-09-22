5d2cf4214fcc0f5c44fe4161731967c1f44b9db0
Casey Haaland <caseychandlerh@yahoo.com>
2018-09-22 14:40:25 -0700

    Finish HW2

--- HW2/src/projected_sgd_lasso.py
+++ HW2/src/projected_sgd_lasso.py
@@ -2,10 +2,25 @@ import numpy as np
 import pdb
 from numpy import zeros, ones
 
-def projected_batch_grad_desc_lasso(X, y, alpha=0.1, lambda_reg=1, num_iter=1000):
-    num_instances, num_features = X.shape
-    theta_pos = 0.1 * np.random.randn(num_features) 
-    theta_neg = 0.1 * np.random.randn(num_features)
+def projected_batch_grad_desc_lasso(X, y, theta_init=None, alpha=0.1, lambda_reg=1, num_iter=1000):
+    _, num_features = X.shape
+    
+    if theta_init is None:
+        theta_pos = 0.1 * np.random.randn(num_features) 
+        theta_neg = 0.1 * np.random.randn(num_features)
+    else:
+        theta_pos = theta_init.clip(min=0)
+        theta_neg = (-1.0 * theta_init).clip(min=0)
+
+    eta0 = 0.00005
+    if isinstance(alpha, float):
+        alpha_func = lambda x : alpha
+    elif alpha == 'inv':
+        alpha_func = lambda x: eta0/x
+    elif alpha == 'invsqrt':
+        alpha_func = lambda x: eta0/np.sqrt(x)
+    else:
+        raise ValueError(str) 
     loss_hist = zeros(num_iter)
     theta_hist = zeros((num_iter, num_features))
 
@@ -14,11 +29,12 @@ def projected_batch_grad_desc_lasso(X, y, alpha=0.1, lambda_reg=1, num_iter=1000
         theta = theta_pos - theta_neg
         r = X.dot(theta) - y
         theta_pos_grad_step =  2 * np.dot(X.T, r) + lambda_reg * ones(num_features)
-        theta_neg_grad_step = -theta_pos_grad_step
+        theta_neg_grad_step = -2 * np.dot(X.T, r) + lambda_reg * ones(num_features)
+        a = alpha_func(i+1)
 
         # take gradient step
-        theta_pos = theta_pos - alpha * theta_pos_grad_step
-        theta_neg = theta_neg - alpha * theta_neg_grad_step
+        theta_pos = theta_pos - a * theta_pos_grad_step
+        theta_neg = theta_neg - a * theta_neg_grad_step
 
         # project onto nonnegative orthant
         theta_pos = np.maximum(theta_pos, np.zeros_like(theta_pos))
@@ -32,7 +48,7 @@ def projected_batch_grad_desc_lasso(X, y, alpha=0.1, lambda_reg=1, num_iter=1000
     return theta_hist, loss_hist
 
 
-def projected_sgd_lasso(X, y, alpha=0.1, lambda_reg=1, num_iter=1000):
+def projected_sgd_lasso(X, y, theta_init=None, alpha=0.1, lambda_reg=1, num_iter=1000):
     """
     Implement stochastic gradient descent for the lasso objective
 
@@ -53,15 +69,20 @@ def projected_sgd_lasso(X, y, alpha=0.1, lambda_reg=1, num_iter=1000):
         num_iter - number of epochs (i.e number of times) to go through the whole training set
 
     Returns:
-        theta_hist - the history of parameter vector, 3D numpy array of size (num_iter, num_instances, num_features)
-        loss_hist - the history of regularized loss function vector, 2D numpy array of size(num_iter, num_instances)
+        theta_hist - the history of parameter vector, 3D numpy array of size (num_iter, num_features)
+        loss_hist - the history of regularized loss function vector, 2D numpy array of size(num_iter)
     """
 
     num_instances, num_features = X.shape
-    theta_pos = 0.1 * np.random.randn(num_features) 
-    theta_neg = 0.1 * np.random.randn(num_features)
-    loss_hist = zeros((num_iter, num_instances))
-    theta_hist = zeros((num_iter, num_instances, num_features))
+    if theta_init is None:
+        theta_pos = 0.1 * np.random.randn(num_features) 
+        theta_neg = 0.1 * np.random.randn(num_features)
+    else:
+        theta_pos = theta_init.clip(min=0)
+        theta_neg = (-1.0 * theta_init).clip(min=0)
+
+    loss_hist = zeros(num_iter)
+    theta_hist = zeros((num_iter, num_features))
 
     indices = np.arange(num_instances)
 
@@ -69,12 +90,11 @@ def projected_sgd_lasso(X, y, alpha=0.1, lambda_reg=1, num_iter=1000):
     for i in range(num_iter):
         np.random.shuffle(indices)
         for j, index in enumerate(indices):
-            # pdb.set_trace()
             x = X[index,:]
             theta = theta_pos - theta_neg
             residual = np.dot(x, theta) - y[index] 
             theta_pos_sgd_step = 2 * residual * x + lambda_reg * ones(num_features)
-            theta_neg_sgd_step = -theta_pos_sgd_step
+            theta_neg_sgd_step = -2 * residual * x + lambda_reg * ones(num_features)
 
             # take gradient step
             theta_pos = theta_pos - alpha * theta_pos_sgd_step
@@ -83,12 +103,12 @@ def projected_sgd_lasso(X, y, alpha=0.1, lambda_reg=1, num_iter=1000):
             # project onto nonnegative orthant
             theta_pos = np.maximum(theta_pos, np.zeros_like(theta_pos))
             theta_neg = np.maximum(theta_neg, np.zeros_like(theta_neg))
-
-            theta_hist[i,j,:] = (theta_pos - theta_neg)
-            regularization_loss = np.linalg.norm(theta_hist[i,j,:], ord=1)
-            data_loss = compute_square_loss(X, y, theta_hist[i,j,:])
-            loss_hist[i,j] = data_loss + lambda_reg * regularization_loss
             cnt += 1
+
+        theta_hist[i,:] = (theta_pos - theta_neg)
+        regularization_loss = np.linalg.norm(theta_hist[i,:], ord=1)
+        data_loss = compute_square_loss(X, y, theta_hist[i,:])
+        loss_hist[i] = data_loss + lambda_reg * regularization_loss
 
     return theta_hist, loss_hist
 
--- HW2/src/shooting_algo.py
+++ HW2/src/shooting_algo.py
@@ -2,7 +2,7 @@ import numpy as np
 import pdb
 
 
-def shooting_algo(X, y, theta=None, lambda_reg=1, max_iter=1000, tol=1e-8, method='rand'):
+def shooting_algo(X, y, theta_init=None, lambda_reg=1, max_iter=1000, tol=1e-8, method='rand'):
     """
     Implement stochastic gradient descent for the lasso objective
 
@@ -20,21 +20,22 @@ def shooting_algo(X, y, theta=None, lambda_reg=1, max_iter=1000, tol=1e-8, metho
 
     Returns:
         theta_hist - the history of parameter vector, 3D numpy array of size (num_iter, num_features)
-        loss_hist - the history of regularized loss function vector, 2D numpy array of size(num_iter, num_instances)
+        loss_hist - the history of regularized loss function vector, 2D numpy array of size (num_iter,)
     """
 
     n, d = X.shape
-    loss_hist = np.zeros((max_iter, d))
+    loss_hist = np.zeros(max_iter)
     theta_hist = np.zeros((max_iter, d))
 
-    if theta is None:
+    if theta_init is None:
         A = np.vstack([X, np.sqrt(lambda_reg) * np.eye(d)])
         b = np.vstack([y[:,None], np.zeros((d,1))])
-        theta = np.linalg.lstsq(A, b)[0].squeeze()
+        theta = np.linalg.lstsq(A, b, rcond=None)[0].squeeze()
     else:
-        theta = np.zeros(d)
-    
+        theta = theta_init
+
     indices = np.arange(d)
+    prev_loss = compute_square_loss(X, y, theta) 
     for iteration in range(max_iter):
         if method.lower() == 'rand':
             np.random.shuffle(indices)
@@ -47,35 +48,62 @@ def shooting_algo(X, y, theta=None, lambda_reg=1, max_iter=1000, tol=1e-8, metho
                 theta[coord] = 0
             else:
                 theta[coord] = soft_threshold(c / a, lambda_reg / a)
-            squared_loss = compute_square_loss(X, y, theta)
-            regularization_loss = np.linalg.norm(theta, ord=1)
-            loss_hist[iteration, index] = squared_loss + lambda_reg * regularization_loss
+
+        squared_loss = compute_square_loss(X, y, theta)
+        regularization_loss = np.linalg.norm(theta, ord=1)
+        loss_hist[iteration] = squared_loss + lambda_reg * regularization_loss
         theta_hist[iteration, :] = theta
 
-        if np.abs(loss_hist[iteration, 0] - loss_hist[iteration,d-1]) <= tol:
+        if np.abs(loss_hist[iteration] - prev_loss) <= tol:
             break
+
+        prev_loss = loss_hist[iteration]
+
     return theta_hist[:iteration+1,:], loss_hist[:iteration+1]
     
 
 def soft_threshold(x, y):
+    '''Returns the subgradient of the LASSO objective function
+
+    Args : 
+        x : float
+        y : float
+    
+    Returns
+        float
+    '''
     return np.sign(x) * np.maximum(np.abs(x) - y, 0)
 
 def compute_square_loss(X, y, theta):
+    ''' Compute the sum of squared errors
+    '''
     return np.sum(np.square(X.dot(theta) - y))
 
-def lasso_regularization_path(X, y, lambdas=None, max_iter=1000):
+def lasso_regularization_path(X_train, y_train, X_val, y_val, lambdas=None, max_iter=1000):
+    '''Compute the validation loss for different values of the regularization parameter
+
+    Args :
+        X_train : ndarray
+            Feature matrix of size (num_instances, num_features)
+        y_train : ndarray
+            Instance labels of size (num_instances,)
+        X_val :
+            Validation feature matrix
+        y_val :
+            Validation labels
+
+    Returns :
+        ndarray
+    '''
     if lambdas is None:
-        lambda_max = 2 * np.linalg.norm(np.dot(X.T, y), ord=np.inf)
+        lambda_max = 2 * np.linalg.norm(np.dot(X_train.T, y_train), ord=np.inf)
         lambdas = 10 ** np.linspace(-6, np.log10(lambda_max), 10)
     
     prev_theta = None
     loss_traj = []
     for index, reg_param in enumerate(lambdas):
-        theta_hist, loss_hist = shooting_algo(X, y, theta=prev_theta, lambda_reg=reg_param)
+        theta_hist, loss_hist = shooting_algo(X_train, y_train, theta_init=prev_theta, lambda_reg=reg_param)
+        loss_traj.append(compute_square_loss(X_val, y_val, theta_hist[-1,:]))
+        prev_theta = theta_hist[-1,:]
 
-        print('Iter: {0}  Loss:{1}'.format(index, loss_hist.shape)) 
-        pdb.set_trace()
-        # prev_theta = theta_hist[-1,:]
-        loss_traj.append(loss_hist[-1,-1])
-
-    return lambdas, loss_traj
+    return loss_traj
