39e167d0d306509f34a03afa25df9c99d20ad25b
Casey Haaland <caseychandlerh@yahoo.com>
2019-05-06 20:09:56 +0100

    Style changes

--- HW6/src/GradientBoosting.py
+++ HW6/src/GradientBoosting.py
@@ -1,6 +1,6 @@
 class GradientBoosting:
-    """
-    Gradient Boosting regressor class
+    """Gradient Boosting regressor class
+
     :method fit: fitting model
     """
 
@@ -12,12 +12,13 @@ class GradientBoosting:
         min_sample=5,
         max_depth=3,
     ):
-        """
-        Initialize gradient boosting class
+        """Initialize gradient boosting class
         
         :param n_estimator: number of estimators (i.e. number of rounds of gradient boosting)
         :pseudo_residual_func: function used for computing pseudo-residual
         :param learning_rate: step size of gradient descent
+        :param min_sample: minimum number of samples in a leaf
+        :param max_depth: maximum depth of the tree
         """
         self.n_estimator = n_estimator
         self.pseudo_residual_func = pseudo_residual_func
@@ -26,13 +27,16 @@ class GradientBoosting:
         self.max_depth = max_depth
 
     def fit(self, train_data, train_target):
-        """
-        Fit gradient boosting model
+        """Fit gradient boosting model
+
+        :param train_data:
+        :param train_target:
         """
         # Your code goes here
 
     def predict(self, test_data):
-        """
-        Predict value
+        """Predict value
+
+        :param test_data:
         """
         # Your code goes here
